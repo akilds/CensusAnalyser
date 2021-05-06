ad1b3e6a80317d7f7be3126f22296ff43f8bb8eb
akilds <akilrainadesigan@gmail.com>
2021-05-06 12:46:39 +0530

    INDIAN CENSUS ANALYSER

---
+++ IndianCensus.java
@@ -0,0 +1,47 @@
+public class IndianCensus
+{
+    private String stateName;
+    private String population;
+    private double maleLiterate;
+    private double femaleLiterate;
+
+    public String getStateName()
+    {
+        return stateName;
+    }
+
+    public void setStateName(String stateName)
+    {
+        this.stateName = stateName;
+    }
+
+    public String getPopulation()
+    {
+        return population;
+    }
+
+    public void setPopulation(String population)
+    {
+        this.population = population;
+    }
+
+    public double getMaleLiterate()
+    {
+        return maleLiterate;
+    }
+
+    public void setMaleLiterate(double maleLiterate)
+    {
+        this.maleLiterate = maleLiterate;
+    }
+
+    public double getFemaleLiterate()
+    {
+        return femaleLiterate;
+    }
+
+    public void setFemaleLiterate(double femaleLiterate)
+    {
+        this.femaleLiterate = femaleLiterate;
+    }
+}
---
+++ IndianCensusAnalyserTest.java
@@ -0,0 +1,38 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class IndianCensusAnalyserTest
+{
+    private static final String ORIGINAL_FILE_PATH = "Desktop\\IndianCensus\\main\\india-districts-census-2011.csv";
+    private static final String WRONG_FILE_PATH = "Desktop\\IndianCensus\\test\\india-districts-census-2011.csv";
+
+    @Test
+    public void givenCorrectFilePathPassTheTest()
+    {
+        try
+        {
+            IndianCensusAnalyser censusAnalyser = new IndianCensusAnalyser();
+            int numOfRecords = censusAnalyser.loadIndiaCensusData(ORIGINAL_FILE_PATH);
+            Assert.assertEquals(640, numOfRecords);
+        }
+        catch (CensusAnalyserException e) {}
+    }
+
+    @Test
+    public void givenWrongPathReturnsCustomException()
+    {
+        try
+        {
+            IndianCensusAnalyser censusAnalyser = new IndianCensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            int numOfRecords = censusAnalyser.loadIndiaCensusData(WRONG_FILE_PATH);
+            Assert.assertEquals(640, numOfRecords);
+        }
+        catch (CensusAnalyserException e)
+        {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
+        }
+    }
+}
